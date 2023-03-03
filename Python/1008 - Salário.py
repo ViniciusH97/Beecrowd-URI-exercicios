66c5967d9508caf071604ffef339ce4b22229d8a
Vinicius <vinicius.hl2010@gmail.com>
2023-03-03 11:02:32 -0300

    .

---
+++ Python/1008 - Salário.py
@@ -0,0 +1,8 @@
+num = int(input())
+hrs = int(input())
+
+dollforhrs = float(input())
+
+salary = dollforhrs * hrs
+
+print("NUMBER = {}\nSALARY = U$ {:.2f}".format(num,salary))
---
+++ Python/1009 - Salário com Bônus.py
@@ -0,0 +1,9 @@
+nome = str(input())
+salafix = float(input())
+totalven = float(input())
+
+total = totalven * 0.15
+
+total = salafix + total
+
+print("TOTAL = R$ {:.2f}".format(total))
