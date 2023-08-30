ff4b9b13ce46813b6868703c2c88ccbca1218252
Vinicius H. Lima <106411366+ViniciusH97@users.noreply.github.com>
2023-08-30 12:03:55 +0000

    .

--- C-C++/1005 - Média 1.c
+++ C-C++/1005 - Média 1.c
@@ -2,11 +2,11 @@
 
 int main()
 {
-   double a,b,c;
+   double a,b;
    
    scanf("%lf%lf",&a,&b);
-   int m = (a*3.5+b*7.5)/11;
-   printf("MEDIA = %.5lf\n",m);
+   double m = (a*3.5+b*7.5)/11;
+   printf("MEDIA = %lf\n",m);
    
    return 0;
     
