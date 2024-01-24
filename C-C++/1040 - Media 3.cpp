b24bdb0be27c0e68c307aa72a15f65e5d4e1f9b5
ViniciusH97 <vinicius.hl2010@gmail.com>
2024-01-23 23:13:33 -0300

    não finalizado

---
+++ C-C++/1040 - Media 3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iomanip>
+
+int main(){
+
+    double nota[4], media, nota_exame;
+    int i;
+    for(i=0; i<4; i++){
+        std::cin>>nota[i];
+    }
+
+    double t = (nota[0] * 2)+ (nota[1] * 3) + (nota[2] * 4)+ nota[3];
+    media = t/10;
+
+    std::cout<<"Media: " << media << std::endl;
+
+    if (media >= 7.0){
+        std::cout<<"Aluno aprovado."<<std::endl;
+    }else if(media < 5.0){
+        std::cout<<"Aluno reprovado."<<std::endl;
+    }else if(media >= 5.0 && media <= 6.9){
+        std::cout<<"Aluno em exame."<<std::endl;
+
+        std::cin>>nota_exame;
+        std::cout<<"Nota do exame: "<< nota_exame << std::endl;
+
+        media = (media + nota_exame)/2;
+
+        if(media >= 5.0){
+            std::cout<<"Aluno aprovado."<<std::endl;
+        }else if(media >= 4.9){
+            std::cout<<"Aluno reprovado."<<std::endl;
+        }
+
+        std::cout<<"Media final: "<< std::setprecision(2) << media << std::endl;
+    }
+
+    return 0;   
+}
