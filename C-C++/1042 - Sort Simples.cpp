7ab06c2b98af650a02ff8f7e0cc7cc12c29a406e
ViniciusH97 <vinicius.hl2010@gmail.com>
2024-01-24 15:54:15 -0300

    .

---
+++ C-C++/1042 - Sort Simples.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main(){
+
+    std::vector<int> vetor1;
+    std::vector<int> vetor2;
+    int valores;
+
+    for(int i=0; i<3; i++){
+        std::cin>>valores;
+        vetor1.push_back(valores);
+        vetor2.push_back(valores);
+    }
+
+    std::sort(vetor1.begin(), vetor1.end());
+
+    for(int i : vetor1){
+        std::cout<< i << std::endl;
+    }
+    std::cout<<std::endl;
+
+    for(int i : vetor2){
+        std::cout<<i<<std::endl;
+    }
+
+    return 0;
+}
