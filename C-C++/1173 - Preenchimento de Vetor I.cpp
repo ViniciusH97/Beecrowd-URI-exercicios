3de9c8edb17ada2e6ed4414dbc7ee41da3e0642e
ViniciusH97 <vinicius.hl2010@gmail.com>
2024-01-25 15:29:22 -0300

    .

--- C-C++/1173 - Preenchimento de Vetor I.cpp
+++ C-C++/1173 - Preenchimento de Vetor I.cpp
@@ -1,14 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    int N[10], X, i; 
-         scanf("%d", &N[0]);
-         printf("N[0] = %d\n", N[0]);
-         for(i=1;i<10;i++){
-         N[i]=N[i-1]*2;
-            printf("N[%d] = %d\n", i, N[i]);
-        }
+    int N[10], X, i;
+    scanf("%d", &N[0]);
+    printf("N[0] = %d\n", N[0]);
+    for (i = 1; i < 10; i++)
+    {
+        N[i] = N[i - 1] * 2;
+        printf("N[%d] = %d\n", i, N[i]);
+    }
 
     return 0;
 }
