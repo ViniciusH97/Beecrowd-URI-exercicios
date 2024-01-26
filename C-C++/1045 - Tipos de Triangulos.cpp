e89cf17233628cbde67b957ad57fc689fb6c872e
Vinicius Lima <106411366+ViniciusH97@users.noreply.github.com>
2024-01-26 20:37:41 -0300

    .

--- C-C++/1045 - Tipos de Triangulos.cpp
+++ C-C++/1045 - Tipos de Triangulos.cpp
@@ -1,30 +1,71 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-int main(){
+bool verifica_triangulo(float a, float b, float c)
+{
 
-    float a, b, c;
+    if (a >= (b + c))
+    {
+        std::cout << "NAO FORMA TRIANGULO" << std::endl;
+        return false;
+    }
+    else
 
-    std::cin>>a>>b>>c;
+        return true;
+}
 
-    if(a>=(a+c))
-        std::cout<<"NAO FORMA TRIANGULO"<<std::endl;
+int main()
+{
 
-    if((a*a)==((b*b)+(c*c)))
-        std::cout<<"TRIANGULO RETANGULO"<<std::endl;
+    float a, b, c, lado[2];
+    int i;
 
-    if((a*a) > ((b*b) + (c*c)))
-        std::cout<<"TRIANGULO OBTUSANGULO"<<std::endl;
+    std::vector<float> v;
 
-    if((a*a) < ((b*b) + (c*c)))
-        std::cout<<"TRIANGULO ACUTANGULO"<<std::endl;
+    std::cin >> lado[0];
+    std::cin >> lado[1];
+    std::cin >> lado[2];
 
-    if(a == b && b == c && a == c)
-        std::cout<<"TRIANGULO EQUILATERO"<<std::endl;
-    
-    if(a == b && a == c && b != c)
-        std::cout<<"TRIANGULO ISOSCELES"<<std::endl;
-    
+    for (i = 0; i < 3; i++)
+    {
+        v.push_back(lado[i]);
+    }
+    std::sort(v.begin(), v.end(), std::greater<float>());
+
+    a = v[0];
+    b = v[1];
+    c = v[2];
+
+    if (verifica_triangulo(a, b, c))
+    {
+
+        if ((a * a) == ((b * b) + (c * c)))
+            std::cout << "TRIANGULO RETANGULO" << std::endl;
+
+        if ((a * a) > ((b * b) + (c * c)))
+            std::cout << "TRIANGULO OBTUSANGULO" << std::endl;
+
+        if ((a * a) < ((b * b) + (c * c)))
+            std::cout << "TRIANGULO ACUTANGULO" << std::endl;
+
+        if (a == b && b == c && a == c)
+            std::cout << "TRIANGULO EQUILATERO" << std::endl;
+
+        if (a == b && a == c && b != c)
+            std::cout << "TRIANGULO ISOSCELES" << std::endl;
+
+        if (a == b && a != c && !(b == c))
+            std::cout << "TRIANGULO ISOSCELES" << std::endl;
+        
+        if (a != b && a == c && !(b == c))
+            std::cout << "TRIANGULO ISOSCELES" << std::endl;
+            
+    }
+
+    std::cout << "a: " << a << std::endl;
+    std::cout << "b: " << b << std::endl;
+    std::cout << "c: " << c << std::endl;
 
     return 0;
 }
