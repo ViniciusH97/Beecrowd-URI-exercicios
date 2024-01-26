f5beb2b78424711731b4629e5c1b778f0cb19bb7
Vinicius Lima <106411366+ViniciusH97@users.noreply.github.com>
2024-01-26 20:54:12 -0300

    .

--- C-C++/1045 - Tipos de Triangulos.cpp
+++ C-C++/1045 - Tipos de Triangulos.cpp
@@ -4,38 +4,31 @@
 
 bool verifica_triangulo(float a, float b, float c)
 {
-
     if (a >= (b + c))
     {
         std::cout << "NAO FORMA TRIANGULO" << std::endl;
         return false;
     }
     else
-
+    {
         return true;
+    }
 }
 
 int main()
 {
+    std::vector<float> v(3);
 
-    float a, b, c, lado[2];
-    int i;
-
-    std::vector<float> v;
-
-    std::cin >> lado[0];
-    std::cin >> lado[1];
-    std::cin >> lado[2];
-
-    for (i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++)
     {
-        v.push_back(lado[i]);
+        std::cin >> v[i];
     }
+
     std::sort(v.begin(), v.end(), std::greater<float>());
 
-    a = v[0];
-    b = v[1];
-    c = v[2];
+    float a = v[0];
+    float b = v[1];
+    float c = v[2];
 
     if (verifica_triangulo(a, b, c))
     {
@@ -49,23 +42,15 @@ int main()
         if ((a * a) < ((b * b) + (c * c)))
             std::cout << "TRIANGULO ACUTANGULO" << std::endl;
 
-        if (a == b && b == c && a == c)
-            std::cout << "TRIANGULO EQUILATERO" << std::endl;
+        if (a == b || a == c || b == c)
+        {
+            if (a == b && b == c)
+                std::cout << "TRIANGULO EQUILATERO" << std::endl;
+            else
+                std::cout << "TRIANGULO ISOSCELES" << std::endl;
+        }
 
-        if (a == b && a == c && b != c)
-            std::cout << "TRIANGULO ISOSCELES" << std::endl;
-
-        if (a == b && a != c && !(b == c))
-            std::cout << "TRIANGULO ISOSCELES" << std::endl;
-        
-        if (a != b && a == c && !(b == c))
-            std::cout << "TRIANGULO ISOSCELES" << std::endl;
-            
     }
-
-    std::cout << "a: " << a << std::endl;
-    std::cout << "b: " << b << std::endl;
-    std::cout << "c: " << c << std::endl;
 
     return 0;
 }
