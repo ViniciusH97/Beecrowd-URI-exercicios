29a5c1254f273e3750f0723fe15fbced4d72a940
Vinicius Lima <106411366+ViniciusH97@users.noreply.github.com>
2024-01-30 15:55:33 -0300

    Add files via upload

--- C-C++/1052 - Mes.cpp
+++ C-C++/1052 - Mes.cpp
@@ -1,36 +1,18 @@
 #include <iostream>
+#include <array>
 
 int main(void){
 
+    std::array<std::string, 12> meses = {"January", "February", "March", "April", "May", 
+        "June", "July", "August", "September", "October", "November", "December"};
+
     int mes;
 
-    std::cin>>mes;
+    std::cin >> mes;
 
-    if(mes == 1){
-        std::cout<<"January"<<std::endl;
-    }else if( mes == 2){
-        std::cout<<"February"<<std::endl;
-    }else if( mes == 3){
-        std::cout<<"March"<<std::endl;
-    }else if(mes == 4){
-        std::cout<<"April"<<std::endl;
-    }else if(mes == 5){
-        std::cout<<"May"<<std::endl;
-    }else if(mes == 6){
-        std::cout<<"June"<<std::endl;
-    }else if(mes == 7){
-        std::cout<<"July"<<std::endl;
-    }else if(mes == 8){
-        std::cout<<"August"<<std::endl;
-    }else if (mes == 9){
-        std::cout<<"September"<<std::endl;
-    }else if(mes == 10){
-        std::cout<<"October"<<std::endl;
-    }else if(mes == 11){
-        std::cout<<"November"<<std::endl;
-    }else if(mes == 12){
-        std::cout<<"December"<<std::endl;
-    }
+    if(mes >= 1 && mes <= 12){
+        std::cout<< meses[mes - 1]<<std::endl;
+    }else
+        return EXIT_SUCCESS;
     
-
 }
