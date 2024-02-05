cda80ef518ba31ceb1d5973270d236d12368a57a
Vinicius Lima <106411366+ViniciusH97@users.noreply.github.com>
2024-02-04 21:57:33 -0300

    Add files via upload

--- C-C++/1071 - Soma impares consecutivos 1.cpp
+++ C-C++/1071 - Soma impares consecutivos 1.cpp
@@ -1,23 +1,58 @@
 #include <iostream>
 
-int main() {
-    int x, y, soma;
+int main(){
+    int s = 0;
+    int n = 0;
+    int x, y;
+    std::cin >> x;
+    std::cin >> y;
 
-    std::cin>>x>>y;
-    
-    if (x > y) {
-        int aux = x;
-        x = y;
-        y = aux;
-    }
-
-    for (int i=x+1; i<y; ++i) {
-        if (i % 2 != 0) {
-            soma += i;
+    if (x > y){
+        int an, ao;
+        if (x % 2 == 0){
+            if (y % 2 == 0){
+                an = x - 1;
+                ao = y + 1;
+            }else{
+                an = x - 1;
+                ao = y + 2;
+            }
+        }else{
+            if (y % 2 == 0){
+                an = x - 2;
+                ao = y + 1;
+            }
+            else{
+                an = x - 2;
+                ao = y + 2;
+            }
         }
+        n = 1 + (an - ao) / 2;
+        s = static_cast<int>((ao + an) * (n / 2.0));
+    }
+    else if (x < y){
+        int an, ao;
+        if (y % 2 == 0){
+            if (x % 2 == 0){
+                an = y - 1;
+                ao = x + 1;
+            }else{
+                an = y - 1;
+                ao = x + 2;
+            }
+        }else{
+            if (x % 2 == 0){
+                an = y - 2;
+                ao = x + 1;
+            }else{
+                an = y - 2;
+                ao = x + 2;
+            }
+        }
+        n = 1 + (an - ao) / 2;
+        s = static_cast<int>((ao + an) * (n / 2.0));
     }
 
-    std::cout<< soma << std::endl;
-
+    std::cout << s << std::endl;
     return 0;
 }
