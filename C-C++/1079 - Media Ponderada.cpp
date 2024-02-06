5038311830280249347f33a6f9c5b23fede55861
Vinicius Lima <106411366+ViniciusH97@users.noreply.github.com>
2024-02-05 23:44:03 -0300

    Add files via upload

--- C-C++/1079 - Media Ponderada.cpp
+++ C-C++/1079 - Media Ponderada.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
+#include<iomanip>
 
 int main(void){
 
     int q;
+    float media, v1, v2, v3;
 
     std::cin>>q;
+
+    for(int i=1;i<=q;i++){
+        std::cin>>v1>>v2>>v3;
+        media = ((v1*2) + (v2*3) + (v3*5))/10;
+        std::cout<<std::fixed<<std::setprecision(1)<<media<<std::endl;
+    }
 
 }
