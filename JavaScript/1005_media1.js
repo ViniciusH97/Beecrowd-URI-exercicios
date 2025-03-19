b285612bc94f2c22f5d8ce8db1c75c51666ff4ca
Vinicius Lima <106411366+ViniciusH97@users.noreply.github.com>
2025-03-19 08:00:41 -0300

    Updated variables name input_a and input_b

--- JavaScript/1005_media1.js
+++ JavaScript/1005_media1.js
@@ -6,11 +6,11 @@ const rl = readline.createInterface({
 
 });
 
-rl.question('', (inputa) =>{
-    rl.question('', (inputb) =>{
+rl.question('', (input_a) =>{
+    rl.question('', (input_b) =>{
 
-        let a = parseFloat(inputa);
-        let b = parseFloat(inputb);
+        let a = parseFloat(input_a);
+        let b = parseFloat(input_b);
 
         a = a * 3.5;
         b = b * 7.5;
